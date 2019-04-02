4bf182473f5595134ef37664156414d5609e9c9c
bigboss <bigboss@192.168.0.6>
2019-04-01 21:05:07 -0400

    Johns linked list

--- src/challenge_AbstractDataType/LinkedList.java
+++ src/challenge_AbstractDataType/LinkedList.java
@@ -3,13 +3,65 @@ package challenge_AbstractDataType;
 public class LinkedList<T> implements I_LinkedList<T> {
 
 	private I_NodeLinkedList<T> headNode;
-
-	@Override
-	public void appendNode(final I_NodeLinkedList<T> node) {
-		// TODO Auto-generated method stub
-
+	
+	//Constructor (empty list object)
+	LinkedList(){
+	}
+	
+	//Constructor (single head object)
+	LinkedList(I_NodeLinkedList<T> firstNode){
+		headNode = firstNode;
 	}
 
+	@Override
+	public int length() {
+		
+		// Initialize dummy node and iteration index
+		I_NodeLinkedList<T> nodey = headNode;
+		int iterationIndex = 0;
+
+		// iterate through nodes to index.
+		while (nodey != null) {
+			nodey = nodey.getNextNode();
+			iterationIndex++;
+		}
+
+		return iterationIndex;
+	}
+	
+	@Override
+	public boolean isEmpty() {
+		if(this.headNode == null) {return true;}
+		return false;
+	}
+	
+	@Override
+	public I_NodeLinkedList<T> head() {
+		return headNode;
+	}
+	
+	@Override
+	public I_NodeLinkedList<T> tail() {
+		I_NodeLinkedList<T> nodey = headNode;
+		
+		if (nodey == null){
+			return nodey;
+		}
+		
+		if (nodey.getNextNode() == null) {
+			return nodey;
+		}
+		
+		I_NodeLinkedList<T> nextNode = nodey.getNextNode();
+
+		do {
+			nodey = nextNode;
+			nextNode = nextNode.getNextNode();
+		} while (nextNode != null);
+
+		return nodey;
+	}
+	
 	/**
 	 * This method indexes from 0.
 	 * 
@@ -21,120 +73,87 @@ public class LinkedList<T> implements I_LinkedList<T> {
 	public I_NodeLinkedList<T> getNodeAtIndex(final int index) throws InvalidListIndexException {
 
 		// Handle if index is below zero.
-		if (index < 0) {
-			System.out.println("Index may not be negative");
-			return null;
-		}
-		;
-
-		// Initialize dummy node
-		I_NodeLinkedList<T> nodey = headNode;
-
-		// Catch empty LinkedList before processing.
-		if (nodey == null) {
-			System.out.println("No nodes have been initialized!");
-			return nodey;
-		}
-
-		// iterate through nodes to index.
-		for (int i = 0; i < index; i++) {
-			nodey = nodey.getNextNode();
-			// Catch if index has gone out of bounds.
-			if (nodey == null) {
-				System.out.println("the index is out of range.");
-			}
-		}
-
-		// return node.
-		return null;
-	}
-
-	@Override
-	public I_NodeLinkedList<T> head() {
-		return headNode;
-	}
-
-	@Override
-	public void insertNodeAtIndex(final int index, final I_NodeLinkedList<T> node) throws InvalidListIndexException {
-		// Handle if index is below zero.
-		if (index < 0) {
-			System.out.println("Index may not be negative");
+		if ((index < 0) | (index >= this.length())) {
+			throw new InvalidListIndexException(index, this.length());
 		}
 
 		// Initialize dummy node
 		I_NodeLinkedList<T> nodey = headNode;
-
-		// Catch empty LinkedList before processing.
-		if (nodey == null) {
-			System.out.println("No nodes have been initialized!");
-		}
-
-		if (index == 0) {
-			this.headNode = node;
-			node.setNextNode(nodey);
-		}
-
-		// iterate through nodes to index.
-		for (int i = 0; i < (index - 1); i++) {
-			nodey = nodey.getNextNode();
-			nodey.getNextNode();
-			// Catch if index has gone out of bounds.
-			if (nodey == null) {
-				System.out.println("the index is out of range.");
-			}
-		}
-
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public int length() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void setNodeAtIndex(final int index, final I_NodeLinkedList<T> node) throws InvalidListIndexException {
-		// Handle if index is below zero.
-		if (index < 0) {
-			System.out.println("Index may not be negative");
-		}
-
-		// Initialize dummy node
-		I_NodeLinkedList<T> nodey = headNode;
-
-		// Catch empty LinkedList before processing.
-		if (nodey == null) {
-			System.out.println("No nodes have been initialized!");
-		}
-
-		// iterate through nodes to index.
-		for (int i = 0; i < index; i++) {
-			nodey = nodey.getNextNode();
-			// Catch if index has gone out of bounds.
-			if (nodey == null) {
-				System.out.println("the index is out of range.");
-			}
-		}
-
-		// set node at index to passed-in-node.
-		nodey = node;
-	}
-
-	@Override
-	public I_NodeLinkedList<T> tail() {
-		I_NodeLinkedList<T> nodey = headNode;
-
-		while (nodey != null) {
+		
+		for(int i=0; i < index; i++) {
 			nodey = nodey.getNextNode();
 		}
 
 		return nodey;
 	}
+	
+	@Override
+	public void setNodeAtIndex(final int index, final I_NodeLinkedList<T> node) throws InvalidListIndexException {
+		// Handle if index is below zero.
+		if (index < 0 | index>(this.length()) ) {
+			throw new InvalidListIndexException(index,this.length());
+		}
+		
+		I_NodeLinkedList<T> nodey = this.headNode;
+		
+		if(index==0) {
+			if(nodey==null) {
+				this.headNode = node;
+				return;
+			}
+			if(nodey.getNextNode() != null) {
+				node.setNextNode(nodey.getNextNode());
+			}
+			this.headNode = node;
+			return;
+		}
 
+		if ( (index==(this.length()-1)) | (index==this.length()) ) {
+			this.getNodeAtIndex(index-1).setNextNode(node);
+		} else {
+			node.setNextNode(this.getNodeAtIndex(index+1));
+			this.getNodeAtIndex(index-1).setNextNode(node);
+		}
+		
+	}
+	
+	@Override
+	public void insertNodeAtIndex(final int index, final I_NodeLinkedList<T> node) throws InvalidListIndexException {
+		// Handle if index is below zero.
+		if (index < 0) {
+			throw new InvalidListIndexException(index,this.length());
+		}
+
+		// Initialize dummy nodes
+		I_NodeLinkedList<T> nodey = headNode;
+		I_NodeLinkedList<T> nextNode = null;
+
+		if (index == 0) {
+			this.headNode = node;
+			node.setNextNode(nodey);
+			return;
+		} else {
+		// iterate through nodes to index.
+		for (int i = 0; i < (index - 1); i++) {
+			nodey = nodey.getNextNode();
+			nextNode = nodey.getNextNode();
+			// Catch if index has gone out of bounds.
+			if (nodey == null) {
+				throw new InvalidListIndexException(index,this.length());
+			}
+		}
+		nodey.setNextNode(node);
+		if(nextNode==null) {
+			return;
+		} else {
+			node.setNextNode(nextNode);
+		}
+		}
+
+	}
+
+	@Override
+	public void appendNode(final I_NodeLinkedList<T> node) {
+		this.tail().setNextNode(node);
+		}
 }
