17a640c7db6cd1e09e3c8bc6d7cfa19f6a6f2b55
bigboss <bigboss@192.168.0.6>
2019-04-04 22:06:19 -0400

    NodeQueue passed all tests first shot

---
+++ src/challenge_AbstractDataType/NodeQueue.java
@@ -0,0 +1,44 @@
+package challenge_AbstractDataType;
+
+public class NodeQueue<NodeDataType> implements I_NodeQueue<NodeDataType> {
+
+	private NodeDataType mObject;
+	private I_NodeQueue<NodeDataType>  mNextNode;
+	
+	public NodeQueue() {	
+	}
+	
+	public NodeQueue(NodeDataType object) {
+		mObject = object;
+	}
+	
+	public NodeQueue(I_NodeQueue<NodeDataType> nextNode) {
+		mNextNode = nextNode;
+	}
+	
+	public NodeQueue(I_NodeQueue<NodeDataType> nextNode, NodeDataType object) {
+		mObject = object;
+		mNextNode = nextNode;
+	}
+	
+	@Override
+	public NodeDataType getDataObject() {
+		return mObject;
+	}
+
+	@Override
+	public void setDataObject(NodeDataType dataObject) {
+		mObject = dataObject;		
+	}
+
+	@Override
+	public I_NodeQueue<NodeDataType> getNextNode() {
+		return mNextNode;
+	}
+
+	@Override
+	public void setNextNode(I_NodeQueue<NodeDataType> nextNode) {
+		mNextNode = nextNode;
+	}
+
+}
